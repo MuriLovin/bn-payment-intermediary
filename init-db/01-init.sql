bde5267199d48ac9d4d02a83905cebb07b2dedf5
MuriLovin <mu.4dev@gmail.com>
2025-07-14 21:31:37 -0300

    feat: add indexes to payments table for correlation_id and created_at

--- init-db/01-init.sql
+++ init-db/01-init.sql
@@ -8,4 +8,7 @@ CREATE TABLE IF NOT EXISTS payments (
     amount DECIMAL(10,2),
     processor_id SMALLINT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-); 
+);
+
+CREATE INDEX IF NOT EXISTS idx_payments_correlation_id ON payments (correlation_id);
+CREATE INDEX IF NOT EXISTS idx_payments_created_at ON payments (created_at);
